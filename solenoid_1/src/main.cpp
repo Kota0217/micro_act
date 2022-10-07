ec1dc5da3645190db92883f3e974fe116657773c
Kota Ishizeki <kota.hatosan.2000@icloud.com>
2022-10-07 15:20:49 +0900

    変更

--- solenoid_1/src/main.cpp
+++ solenoid_1/src/main.cpp
@@ -4,19 +4,12 @@ int input;
 
 void setup() {
   pinMode(3, OUTPUT);
-
-  //digitalWrite(3, LOW);
   delay(2000);
-  //出力電圧
-  double vol = 3.0;
-
-  input = vol * 255.0 / 5.0;
 
 }
 
 void loop() {
   digitalWrite(3, HIGH);
-  //analogWrite(3, input);
   delay(1000);
 
   //digitalWrite(3, LOW);
