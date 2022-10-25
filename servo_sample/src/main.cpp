cbadc6aaa19e643db024536b5f64907551787f9d
Kota Ishizeki <kota.hatosan.2000@icloud.com>
2022-10-25 16:56:01 +0900

    サーボ1, 2の動作確認済み

--- servo_sample/src/main.cpp
+++ servo_sample/src/main.cpp
@@ -1,31 +1,39 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-Servo servo;//サーボのインスタンス
+Servo servo1;//サーボ1のインスタンス
+//サーボ1の初期位置
+const int s1_init = 90;
+const int s2_init = 90;
+Servo servo2;//サーボ2のインスタンス
+
 
 void setup(){
-  //サーボの信号線を３番ピンに接続
+  //サーボ1の信号線を３番ピンに接続
   //（PWMピン以外のピンにも接続可）
-  servo.attach(3);
+  servo1.attach(3);
+  servo2.attach(5);
 
   //初期位置に移動
-  servo.write(20);
+  servo1.write(s1_init);
+  servo2.write(s2_init);
 
   Serial.begin(115200);
 }
 
 void loop(){
 
-  //サーボ出力
-  servo.write(20);
-  Serial.print("20 degree\n");
+  //初期位置
+  servo1.write(s1_init);
+  //servo2.write(s2_init);
   delay(1000);
-  
-  servo.write(10);//0~180まで
-  Serial.print("10 degree\n");
-  delay(2000);
 
-  servo.write(0);//0~180まで
+  //サーボ出力
+  //+で反時計周り
+  servo1.write(-5+s1_init);
+  //servo2.write(-5+s2_init);
+  Serial.print("10 degree\n");
   delay(1000);
+
 
 }
