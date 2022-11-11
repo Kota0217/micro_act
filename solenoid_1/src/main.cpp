15a6f1dbe326b84396dc119320db95dd56589bfb
Kota Ishizeki <kota.hatosan.2000@icloud.com>
2022-11-11 19:56:57 +0900

    ソレノイドを2つ動かせるように変更

--- solenoid_1/src/main.cpp
+++ solenoid_1/src/main.cpp
@@ -4,16 +4,27 @@ int input;
 
 void setup() {
   pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
   //delay(2000);
 
 }
 
 void loop() {
   digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
   delay(1000);
 
   //digitalWrite(3, LOW);
+  //digitalWrite(4, LOW);
+  delay(500);
+  
+
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
   delay(1000);
 
-
-}
+  //digitalWrite(3, LOW);
+  //digitalWrite(4, LOW);
+  delay(500);
+  
+  }
