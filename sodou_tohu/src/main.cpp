c3a7f9ff592165f1f41e0dc53e1298c38fe85667
Kota Ishizeki <kota.hatosan.2000@icloud.com>
2023-01-10 15:52:39 +0900

    initial commit

--- sodou_tohu/src/main.cpp
+++ sodou_tohu/src/main.cpp
@@ -1,12 +1,20 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+Servo servo1;//サーボ1のインスタンス
+Servo servo2;//サーボ2のインスタンス
+Servo servo3;//サーボ3のインスタンス
+Servo servo4;//サーボ4のインスタンス
 Servo servo5;//サーボ5のインスタンス
 Servo servo6;//サーボ6のインスタンス
 
 
 
 //サーボ1の初期位置
+const int s1_init = 90;
+const int s2_init = 90;
+const int s3_init = 90;
+const int s4_init = 90;
 const int s5_init = 90;
 const int s6_init = 90;
 
@@ -18,8 +26,11 @@ void setup(){
   //サーボ1の信号線を３番ピンに接続
   //（PWMピン以外のピンにも接続可）
 
+  servo1.attach(4);
+  servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(12);
   servo5.attach(8);
-  servo6.attach(3);
 
   //******************初期位置へ移動*************************
   //サーボ出力
@@ -27,9 +38,10 @@ void setup(){
   //servo5.write(-5+s5_init);
   delay(1000);
 
+  servo1.write(-10+s1_init);
+  servo3.write(-10+s3_init);
 
-  servo5.write(s5_init);
-  servo6.write(s5_init);
+  servo5.write(-5+s5_init);
   delay(2000);
 
   /*
@@ -46,39 +58,44 @@ void setup(){
   */
 
 /**************************************************************/
+  //対象物付近へ移動
+  servo1.write(-15+s1_init);
+  servo3.write(-25+s3_init);
 
+  delay(1000);
 
+  //加工用マニピュレータの加工動作
   servo5.write(2+s5_init);
-  servo6.write(-2+s5_init);
+  //servo6.write(-2+s5_init);
   delay(2000);
 
   servo5.write(4+s5_init);
-  servo6.write(-4+s5_init);
+  //servo6.write(-4+s5_init);
   delay(2000);
 
   servo5.write(6+s5_init);
-  servo6.write(-6+s5_init);
+  //servo6.write(-6+s5_init);
   delay(2000);
 
   servo5.write(8+s5_init);
-  servo6.write(-8+s5_init);
+  //servo6.write(-8+s5_init);
   delay(2000);
   
   
   servo5.write(10+s5_init);
-  servo6.write(-10+s5_init);
+  //servo6.write(-10+s5_init);
   delay(2000);
 
   servo5.write(12+s5_init);
-  servo6.write(-12+s5_init);
+  //servo6.write(-12+s5_init);
   delay(2000);
 
   servo5.write(14+s5_init);
-  servo6.write(-14+s5_init);
+  //servo6.write(-14+s5_init);
   delay(2000);
 
   servo5.write(16+s5_init);
-  servo6.write(-16+s5_init);
+  //servo6.write(-16+s5_init);
   delay(2000);
 //*/
 
